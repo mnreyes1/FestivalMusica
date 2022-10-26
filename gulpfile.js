a5ad893dc566a2f06ae7a71d78c1f994e693adf6
benjamin_reyes_meza <68304573+berryterreble@users.noreply.github.com>
2022-10-26 11:44:31 -0300

    added plumber and import on scss, added header

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 const {src, dest, watch} = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require('gulp-plumber');
 
 function css(done) {
-    src("src/sass/app.scss").pipe(sass()).pipe(dest("build/css"))
+    src("src/scss/**/*.scss") // Identifica el/los archivo/s .scss a compilar
+    .pipe(plumber()) // Para que no detenga ejecucion cuando hayan errores y aparezca error formateado
+    .pipe(sass()) // Compila
+    .pipe(dest("build/css")); // Almacena en disco duro (crea archivo css)
     done();
 }
 
 function dev(done) {
-    watch("src/sass/app.scss", css);
+    watch("src/scss/**/*.scss", css);
     done();
 }
 
