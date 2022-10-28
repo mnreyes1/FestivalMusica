d33bb01bc78401d2b043600480e2b3c09c82e134
benjamin_reyes_meza <68304573+berryterreble@users.noreply.github.com>
2022-10-28 13:03:09 -0300

    transform imgenes  a webp y creada tarea en gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-const {src, dest, watch} = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+const {src, dest, watch, parallel} = require('gulp');
+
+// CSS
+const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+// Imagenes
+const webp = require('gulp-webp')
+
+// Tareas
 
 function css(done) {
     src("src/scss/**/*.scss") // Identifica el/los archivo/s .scss a compilar
@@ -10,9 +17,22 @@ function css(done) {
     done();
 }
 
+function webpTransform(done) {
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+    done();
+}
+
 function dev(done) {
     watch("src/scss/**/*.scss", css);
     done();
 }
 
+exports.css = css;
+exports.webpTransform = webpTransform;
 exports.dev = dev;
+exports.webp_dev = parallel(webpTransform, dev);
