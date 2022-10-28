88c4d49c13211d510b37bd8af528ed162c215912
benjamin_reyes_meza <68304573+berryterreble@users.noreply.github.com>
2022-10-28 14:18:48 -0300

    agregue optimizacion de imagenes en gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,26 @@ const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
 // Imagenes
-const webp = require('gulp-webp')
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 // Tareas
-
 function css(done) {
-    src("src/scss/**/*.scss") // Identifica el/los archivo/s .scss a compilar
+    src('src/scss/**/*.scss') // Identifica el/los archivo/s .scss a compilar
     .pipe(plumber()) // Para que no detenga ejecucion cuando hayan errores y aparezca error formateado
     .pipe(sass()) // Compila
-    .pipe(dest("build/css")); // Almacena en disco duro (crea archivo css)
+    .pipe(dest('build/css')); // Almacena en disco duro (crea archivo css)
+    done();
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'));
     done();
 }
 
@@ -28,11 +39,12 @@ function webpTransform(done) {
 }
 
 function dev(done) {
-    watch("src/scss/**/*.scss", css);
+    watch('src/scss/**/*.scss', css);
     done();
 }
 
 exports.css = css;
+exports.imagenes = imagenes
 exports.webpTransform = webpTransform;
 exports.dev = dev;
-exports.webp_dev = parallel(webpTransform, dev);
+exports.par_dev = parallel(imagenes, webpTransform, dev);
